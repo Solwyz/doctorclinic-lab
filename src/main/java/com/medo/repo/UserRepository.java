26f3b9c4d81a761762da631f018eea31d9122bc5
Solwyz <jismis@solwyz.com>
2025-03-03 09:37:01 +0530

    changed

--- src/main/java/com/medo/repo/UserRepository.java
+++ src/main/java/com/medo/repo/UserRepository.java
@@ -3,9 +3,11 @@ package com.medo.repo;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.medo.entity.User;
 
+@Repository
 public interface UserRepository extends JpaRepository<User,Long>{
 
 	User save(User newUser);
