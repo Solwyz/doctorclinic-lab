0f9ca1edb652e21813da0fda3046d6223b7c2ad8
Solwyz <jismis@solwyz.com>
2025-03-04 10:33:19 +0530

    sample removed

--- src/main/java/com/medo/controller/PatientController.java
+++ src/main/java/com/medo/controller/PatientController.java
@@ -76,8 +76,7 @@ public class PatientController {
 	    }
 	
 
-	//notification
-	 ///sample
+	//notification        
 	@GetMapping("/notification/{id}")
 	public ResponseEntity<List<Notification>> getNotification(@PathVariable Long id){
 		List<Notification>notifications=patientService.getNotification(id);
